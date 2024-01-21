7c438c5fde7f6b04a6e03bb5649f31db6c3eb274
Guillermo <ggjnez92@gmail.com>
2024-01-21 12:36:40 +0100

    Initial commit

---
+++ core/inc/library.h
@@ -0,0 +1,16 @@
+#ifndef JS_SCRIPT_AI_LIBRARY_H
+#define JS_SCRIPT_AI_LIBRARY_H
+
+namespace Jsai {
+    class Scanner {
+    public:
+        Scanner();
+    };
+
+    class Parser {
+    public:
+        Parser();
+    };
+}
+
+#endif //JS_SCRIPT_AI_LIBRARY_H
---
+++ core/src/library.cpp
@@ -0,0 +1,11 @@
+#include "../inc/library.h"
+
+#include <iostream>
+
+Jsai::Scanner::Scanner() {
+
+}
+
+Jsai::Parser::Parser() {
+
+}
